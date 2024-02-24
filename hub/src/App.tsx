67be772e267c12823c4ebbc02c1e39d5c58d846f
JupiterPi <jupiterpi@outlook.de>
2024-02-24 16:56:03 +0100

    setup react app

---
+++ hub/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hi, I'm JupiterPi</h1>
+      <p>
+        Welcome.
+      </p>
+    </>
+  );
+}
+
+export default App;
