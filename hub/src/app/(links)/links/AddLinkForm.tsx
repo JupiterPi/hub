d1fa645da7676dbdd484f87852c7f2b862ad2a02
JupiterPi <jupiterpi@outlook.de>
2024-02-29 20:39:25 +0100

    polished AddLinkForm

--- hub/src/app/(links)/links/AddLinkForm.tsx
+++ hub/src/app/(links)/links/AddLinkForm.tsx
@@ -1,33 +1,37 @@
 "use client";
 
 import {createLink} from "./actions";
-import {useRef, useState} from "react";
+import {useState} from "react";
 import "@/util/useEphemeralState";
 import useEphemeralState from "@/util/useEphemeralState";
 
 export function AddLinkForm() {
-  const ref = useRef<HTMLFormElement>(null);
   const [status, setStatus] = useEphemeralState<{success: boolean, message: string} | null>(null, 3000);
 
   const [link, setLink] = useState("");
   const [url, setUrl] = useState("");
 
+  const submit = async () => {
+    const status = await createLink({link, url});
+    setStatus(status);
+    if (status.success) {
+      setLink("");
+      setUrl("");
+    }
+  };
+
   return (
-    <form ref={ref} action={async () => {
-      const status = await createLink({link, url});
-      if (status.success) ref.current?.reset();
-      setStatus(status);
-    }}>
+    <div style={{display: "flex", flexDirection: "column", gap: 10}}>
       <div>
         <label>Link: </label>
-        <input onChange={e => setLink(e.target.value)} />
+        <input value={link} onChange={e => setLink(e.target.value)} />
       </div>
       <div>
         <label>URL: </label>
-        <input type="url" onChange={e => setUrl(e.target.value)} />
+        <input type="url" value={url} onChange={e => setUrl(e.target.value)} />
       </div>
-      <button type="submit" style={{alignSelf: "center"}}>Add</button>
+      <button style={{alignSelf: "center"}} onClick={submit}>Add</button>
       {status != null && <div style={{alignSelf: "center", color: status.success ? "green" : "red"}}>{status.message}</div>}
-    </form>
+    </div>
   );
 }
